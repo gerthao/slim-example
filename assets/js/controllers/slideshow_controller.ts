44d29f26cd839451884c19538fc0d57287f10d23
Ger Thao <g.thao19@gmail.com>
2024-03-15 20:41:08 -0500

    switch to typescript over javascript

--- assets/js/controllers/slideshow_controller.ts
+++ assets/js/controllers/slideshow_controller.ts
@@ -1,26 +1,29 @@
 import {Controller} from '@hotwired/stimulus';
 
-export default class extends Controller {
+export default class extends Controller<HTMLDivElement> {
     static targets = ['slide'];
     static values = {index: Number, default: 0};
 
-    next() {
+    declare indexValue: number;
+    declare readonly slideTargets: HTMLElement[];
+
+    next(): void {
         if (this.indexValue < (this.slideTargets.length - 1)) {
             this.indexValue++;
         }
     }
 
-    previous() {
+    previous(): void {
         if (this.indexValue > 0) {
             this.indexValue--;
         }
     }
 
-    indexValueChanged() {
+    indexValueChanged(): void {
         this.showCurrentSlide();
     }
 
-    showCurrentSlide() {
+    showCurrentSlide(): void {
         this.slideTargets.forEach((element, index) => {
             element.hidden = index !== this.indexValue;
         });
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        home: './assets/js/home/home.js',
+        home: './assets/js/home/home.ts',
     },
     output: {
         filename: '[name].bundle.js',
@@ -11,7 +11,18 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/, // This rule applies to files with a .js extension
+                test: /\.ts$/, // This rule applies to typescript files (.ts)
+                exclude: /node_modules/, // Exclude files in the node_modules directory
+                use: [
+                    {
+                        loader: 'babel-loader', // Use babel-loader to transpile JavaScript files
+                        options: {presets: ['@babel/preset-env']} // Use @babel/preset-env for transpilation
+                    },
+                    'ts-loader' // Use ts-loader for transpilation
+                ]
+            },
+            {
+                test: /\.js$/, // This rule applies to javascript files (.js)
                 exclude: /node_modules/, // Exclude files in the node_modules directory
                 use: {
                     loader: 'babel-loader', // Use babel-loader to transpile JavaScript files
@@ -25,6 +36,9 @@ module.exports = {
                 use: ['style-loader', 'css-loader'] // Use style-loader and css-loader for handling CSS files
             }
         ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
     }
 
 }
